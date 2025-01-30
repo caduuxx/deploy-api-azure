3b6b4b3413109042027d209ac56a3febba4159aa
caduuxx <115509827+caduuxx@users.noreply.github.com>
2025-01-30 12:31:54 -0300

    Add files via upload

---
+++ deploy-api-azure-main/main.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => Results.Json(new { message = "Hello, Azure!" }));
+
+app.Run();
